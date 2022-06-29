417b02b6754617e670306bc23b97cac726f97735
Kyle Li <kcao2003@gmail.com>
2022-06-29 03:15:39 -0400

    feat: complete twilio feature and jar executable

--- src/com/kyleli/factsms/Main.java
+++ src/com/kyleli/factsms/Main.java
@@ -1,12 +1,41 @@
 package com.kyleli.factsms;
 
+
+import com.google.common.io.Resources;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        FactGenerator gen = new FactGenerator(FactGenerator.LANG.EN);
+        JSONObject config;
         try {
-            System.out.println(gen.getFact());
+            String configStr = Resources.toString(
+                    Objects.requireNonNull(Main.class.getResource("/config.json")),
+                    StandardCharsets.UTF_8
+            );
+            config = new JSONObject(configStr);
+        } catch (IOException e) {
+            System.out.println("Could not find config file.");
+            System.exit(1);
+            return;
+        }
+
+        FactGenerator gen = new FactGenerator(FactGenerator.LANG.EN);
+        SMSManager sms = new SMSManager();
+
+        try {
+            String fact = gen.getFact();
+            JSONArray numbers = config.getJSONArray("numbers");
+            for (Object number : numbers) {
+                sms.sendText(fact, (String) number);
+            }
         } catch (Exception e) {
             System.out.println("Failed to get fact: " + e.getMessage());
+            System.exit(1);
         }
     }
 }
--- src/com/kyleli/factsms/SMSManager.java
+++ src/com/kyleli/factsms/SMSManager.java
@@ -1,4 +1,23 @@
 package com.kyleli.factsms;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
 public class SMSManager {
+    private static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+    private static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+
+    public SMSManager() {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    }
+
+    public void sendText(String body, String phone) {
+        Message.creator(
+                        new PhoneNumber(phone),
+                        new PhoneNumber(System.getenv("TWILIO_NUMBER")),
+                        body
+                )
+                .create();
+    }
 }
